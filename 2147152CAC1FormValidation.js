bd0805d11d494f74f3086728679364939db15839
2147152SaumyaThukral <96261459+2147152SaumyaThukral@users.noreply.github.com>
2022-02-01 10:09:49 +0530

    Update 2147152CAC1FormValidation.js

--- 2147152CAC1FormValidation.js
+++ 2147152CAC1FormValidation.js
@@ -173,6 +173,32 @@ checkName = (id) => {
         return sum;
     }
 
+    const validate = el => {
+        const checkboxes = el.querySelectorAll('input[type="checkbox"]');
+        return [...checkboxes].some(e => e.checked);
+      };
+      
+      const formEl = document.querySelector("form");
+      const statusEl = formEl.querySelector(".status-message");
+      const checkboxGroupEl = formEl.querySelector(".checkbox-group-required");
+      
+      checkboxGroupEl.addEventListener("click", e => {
+        statusEl.textContent = validate(checkboxGroupEl) ? "Valid" : "Invalid";
+      });
+      
+      formEl.addEventListener("submit", e => {
+        e.preventDefault();
+        
+        if (validate(checkboxGroupEl)) {
+          statusEl.textContent = "";
+          // Send data from e.target to your backend
+        }
+        else {
+          statusEl.textContent = "Error: Select at least one checkbox";
+        }
+      });
+
     function myFunction() {
         document.getElementById("form").reset();
         }
+
